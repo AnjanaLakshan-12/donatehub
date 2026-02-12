f424e080cca87b72793c6c44eb876d71842d2774
NIMNA-DAHAM <vihanga.mayadunnar@gmail.com>
2026-02-12 23:50:18 +0530

    security imports

--- backend/donatehub/src/main/java/org/example/donatehub/security/SecurityConfig.java
+++ backend/donatehub/src/main/java/org/example/donatehub/security/SecurityConfig.java
@@ -1,4 +1,22 @@
 package org.example.donatehub.security;
 
-public class SecurityConfig {
-}
+
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
