d5cfcb568b1db694c13b7cc28c262dc44041d9d1
Abishek Siriwardhana <dulanjanaabishek2003@gmail.com>
2026-02-12 22:59:33 +0530

    update category controller
    category controller created with getall and add new category endpoints

--- backend/donatehub/src/main/java/org/example/donatehub/controller/CategoryController.java
+++ backend/donatehub/src/main/java/org/example/donatehub/controller/CategoryController.java
@@ -1,8 +1,44 @@
 package org.example.donatehub.controller;
 
+import java.util.List;
+
+import org.example.donatehub.entity.Category;
+import org.example.donatehub.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://localhost:5173")
+@RestController
+@RequestMapping("/api/v1")
 public class CategoryController {
 
+     @Autowired
+    private CategoryService categoryService;
+
+
     //get all /api/categories
+    @GetMapping("/categories")
+    public ResponseEntity<List<Category>> getCategories() {
+        List <Category> categories = categoryService.getCategories();
+        if  (categories.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return ResponseEntity.ok(categories);
+    }
+
+
     //add new category   /api/categories
+    @PostMapping("/add/categories")
+    public  ResponseEntity<?> addCategory(@RequestBody Category category) {
+        return categoryService.addCategory(category);
+    }
+
 
 }
