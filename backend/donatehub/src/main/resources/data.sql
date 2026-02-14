7832e775b1b5620386f410b03c91ae7d678c5809
AnjanaLakshan-12 <lganjana12@gmail.com>
2026-02-14 16:13:07 +0530

    application properties update

---
+++ backend/donatehub/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT IGNORE INTO category (name) VALUES
+('Education'),
+('Medical & Health'),
+('Food & Hunger Relief'),
+('Relief'),
+('Environment'),
+('Animal Welfare'),
+('Clothing & Essentials');
